be531dbbf63f17b25dbec2656247a7c0bc79e11b
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-04-16 16:11:12 +0200

    init project

---
+++ src/main/java/com/spring/entity/Book.java
@@ -0,0 +1,28 @@
+package com.spring.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
