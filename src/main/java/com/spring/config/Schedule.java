c591378c8cf55587539d05ee3643143bfb1b9769
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-05-21 10:22:16 +0300

    Schedule job: @ConditionalOnProperty()

--- src/main/java/com/spring/config/Schedule.java
+++ src/main/java/com/spring/config/Schedule.java
@@ -1,13 +1,14 @@
 package com.spring.config;
 
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-
+@ConditionalOnProperty(name ="schedule.enabled", matchIfMissing = true )
 @EnableAsync
 public class Schedule {
 }
