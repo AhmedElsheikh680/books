d316c64687ef3b7f6b26d92f6edcd36440f58371
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-06-12 11:44:22 +0300

    Swagger-ui:Add Categories if you have more than different apis

--- src/main/java/com/spring/config/OpenApiConfig.java
+++ src/main/java/com/spring/config/OpenApiConfig.java
@@ -2,16 +2,23 @@ package com.spring.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -41,7 +48,8 @@ public class OpenApiConfig {
                                 .bearerFormat("JWT")));
 
     }
-    private Contact getContact(){
+
+    private Contact getContact() {
         Contact contact = new Contact();
         contact.setEmail("ahmedelsheikh@gmail.com");
         contact.setName("Book Service");
@@ -58,4 +66,36 @@ public class OpenApiConfig {
 
         return license;
     }
+
+    //    Start Cateegories swagger-ui
+    @Bean
+    public GroupedOpenApi userManagementApi() {
+        String packagesToscan[] = {"com.service.usermanagement"};
+        return GroupedOpenApi.builder()
+                .group("User Management API")
+                .packagesToScan(packagesToscan)
+                .addOperationCustomizer(appTokenHeaderParam())
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi setupApi() {
+        String packagesToscan[] = {"com.spring"};
+        return GroupedOpenApi.builder()
+                .group("Book API")
+                .packagesToScan(packagesToscan)
+                .addOperationCustomizer(appTokenHeaderParam())
+                .build();
+    }
+
+    @Bean
+    public OperationCustomizer appTokenHeaderParam() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            Parameter headerParameter = new Parameter().in(ParameterIn.HEADER.toString()).required(false).
+                    schema(new StringSchema()._default("app_token_header_default_value")).name("app_token_header").description("App Token Header");
+            operation.addParametersItem(headerParameter);
+            return operation;
+        };
+    }
+//    End Categories swagger-ui
 }
