f5b61e984f322c5989857a0d1fe67e4fc1fdbd83
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-10-19 00:09:55 +0300

    profiles

--- src/main/java/com/spring/repo/BookFavorateRepo.java
+++ src/main/java/com/spring/repo/BookFavorateRepo.java
@@ -1,13 +1,13 @@
 package com.spring.repo;
 
+import com.spring.base.BaseRepository;
 import com.spring.entity.BookFavorate;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface BookFavorateRepo extends JpaRepository<BookFavorate, Long> {
+public interface BookFavorateRepo extends BaseRepository<BookFavorate, Long> {
 
     Optional<BookFavorate> findByAuthorIdAndBookId(Long authorId, Long bookId);
 
